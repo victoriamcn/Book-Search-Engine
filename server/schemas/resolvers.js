dca7fdfac77425559a2473a51f2f16bb852a1401
Victoria <vmcnorrill@gmail.com>
2023-06-17 18:57:49 -0400

    starting reolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,10 @@
+// import user model
+const { User } = require('../models');
+// import sign token function from auth
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+
+};
+
+module.exports = resolvers;
