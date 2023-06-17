539470ebb9371990f0b65cf531cb3e89c7896335
Victoria <vmcnorrill@gmail.com>
2023-06-17 18:41:32 -0400

    screated schema folder

---
+++ server/schemas.js/typeDefs.js
@@ -0,0 +1,41 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int!
+    # an array of the Book type
+    savedBooks: [Book]
+}
+type Book {
+    #Not the _id, but the book's id value 
+    #returned from Google's Book API.
+    bookId: String!
+    #An array of strings, as there may be more than one author.
+    authors: [String]
+    description: String!
+    title: String!
+    image: String
+    link: String
+}
+type Auth {
+    token: ID!
+    # References the User type.
+    user: User
+}
+type Query {
+    user(id: ID!): User
+}
+
+type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+
+    saveBook(authors: [String]!, description: String!, title: String!, image, link: ): User
+    removeBook()
+}
+`;
+
+module.exports = typeDefs;
