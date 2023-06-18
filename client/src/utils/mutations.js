41ec41bfe1562ab32f774f50a0b60d69cec5bf3d
Victoria <vmcnorrill@gmail.com>
2023-06-17 20:48:22 -0400

    added mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,9 +1,62 @@
 import { gql } from '@apollo/client';
 
 // LOGIN_USER will execute the loginUser mutation set up using Apollo Server
-
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
 // ADD_USER will execute the addUser mutation
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addProfile(username: $name, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
 
 // SAVE_BOOK will execute the saveBook mutation
+export const SAVE_BOOK = gql`
+  mutation saveBook($authors: [String]!, $description: String!, $title: String!, $bookId: String!, $image: String, $link: String) {
+    saveBook(authors: $authors, description: $description, title: $title, bookId: $bookId, image: $image, link: $link) {
+      _id
+      username
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
 
 // REMOVE_BOOK will execute the removeBook mutation
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
