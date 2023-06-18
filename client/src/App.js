3afb5cade1392b6edd97bcf158c62c4c52ac16c0
Victoria <vmcnorrill@gmail.com>
2023-06-17 21:24:15 -0400

    installed apollo-client npm

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// importing apollo client
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
