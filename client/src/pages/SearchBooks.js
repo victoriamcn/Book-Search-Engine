408ea3f561068934e1c2ec1a1dfb9fb21846f265
Victoria <vmcnorrill@gmail.com>
2023-06-19 19:46:16 -0400

    searchbooks now working

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -94,7 +94,6 @@ const SearchBooks = () => {
         <Container>
           <h1>Search for Books!</h1>
           <Form onSubmit={handleFormSubmit}>
-            <Form.Row>
               <Col xs={12} md={8}>
                 <Form.Control
                   name='searchInput'
@@ -110,7 +109,6 @@ const SearchBooks = () => {
                   Submit Search
                 </Button>
               </Col>
-            </Form.Row>
           </Form>
         </Container>
       </div>
