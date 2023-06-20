11017850b913d60b2eecba5c6dba59dedbe19c9c
Victoria <vmcnorrill@gmail.com>
2023-06-19 21:00:07 -0400

    BUG in sign up form, bad response from apollo/graphQL

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -5,7 +5,6 @@ import { ADD_USER } from '../utils/mutations';
 
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
@@ -15,6 +14,8 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+  //mutation ADD_USER
+  const [addUser] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -32,13 +33,11 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const {data} = await addUser({
+        variables: userFormData
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
+      const { token, user } = data.addUser;
       console.log(user);
       Auth.login(token);
     } catch (err) {
