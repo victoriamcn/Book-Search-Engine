107a56a0fa11a9bbdfee7c5573827d78f521ea33
Victoria <vmcnorrill@gmail.com>
2023-06-21 20:31:15 -0400

    redid npm install. getting Server Error 500 when I run npm run develop

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
   
   db.once('open', () => {
     app.listen(PORT, () => {
-      console.log(`API server running on port http://localhost:${PORT}/.`);
+      console.log(`API server running on port http://localhost:${PORT}.`);
       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}.`);
     })
   })
